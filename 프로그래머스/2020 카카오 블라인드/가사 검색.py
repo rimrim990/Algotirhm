310d7131215399e28d676b1613941a929183e822
rimrim990 <lim990718@naver.com>
2023-11-23 14:54:15 +0900

    [1123] 프로그래머스. 가사 검색, 다른 풀이 추가

--- 프로그래머스/2020 카카오 블라인드/가사 검색.py
+++ 프로그래머스/2020 카카오 블라인드/가사 검색.py
@@ -22,3 +22,54 @@ def solution(words, queries):
     answer.append(right-left)
 
   return answer
+
+def solution_using_trie(words, queries):
+  trie = Trie()
+  rtrie = Trie()
+  for word in words:
+    trie.gen_tree(word)
+    rtrie.gen_tree(word[::-1])
+
+  answer = []
+  for q in queries:
+    key = q.replace('?', '')
+    if q[-1] == '?':
+      answer.append(trie.count_key(key, len(q)))
+    else:
+      answer.append(rtrie.count_key(key[::-1], len(q)))
+
+  return answer
+
+class Trie:
+  N = 10_001
+
+  def __init__(self):
+    self.trie = [{'': [{}, 0]} for _ in range(self.N)]
+
+  def count_key(self, key, n):
+    if key == '':
+      return self.trie[n][''][1]
+
+    ptr = self.trie[n][''][0]
+    for i in range(len(key)):
+      if key[i] not in ptr:
+        return 0
+
+      if i == len(key)-1:
+        return ptr[key[i]][1]
+
+      ptr = ptr[key[i]][0]
+
+  def gen_tree(self, word):
+    n = len(word)
+    self.trie[n][''][1] += 1
+    ptr = self.trie[n][''][0]
+
+    for i in range(n):
+      key = word[i]
+
+      if key not in ptr:
+        ptr[key] = [{}, 0]
+
+      ptr[key][1] += 1
+      ptr = ptr[key][0]
